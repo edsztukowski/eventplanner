000d5bbb715edf85af1f061d4cf096cecbef1a08
EDWARD <edsztukowski@gmail.com>
2018-09-09 12:08:51 -0600

    updated default state to remove test data

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -4,22 +4,7 @@ import { guid } from 'react-agenda';
 var now = new Date();
  
 const eventDefaultState = {
-
     allEvents: [
-        {
-            _id            :guid(),
-            name          : 'Meeting , dev staff!',
-            startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 9, 0),
-            endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0),
-            classes       : 'color-1'
-       },
-       {
-            _id            :guid(),
-            name          : 'Working lunch , Holly',
-            startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 11, 0),
-            endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 13, 0),
-            classes       : 'color-2 color-3'
-       },
     ],
     tempEvent: {}
 }
