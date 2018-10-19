5d145a07520060a726c67615b10397b2e5fe8288
EDWARD <edsztukowski@gmail.com>
2018-10-19 16:39:59 -0600

    fixing ig

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-import * as firebase from "firebase";
-
-import { FirebaseConfig } from "../config/keys";
-firebase.initializeApp(FirebaseConfig);
-
-const database = firebase.database();
-export { firebase, database as default };
