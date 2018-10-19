b828fb9c63bd13effe5ca60896e73ffef20cf17a
Ed Sztukowski <edsztukowski@gmail.com>
2018-10-19 16:28:31 -0600

    Delete dev.js

--- src/config/dev.js
+++
@@ -1,5 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyB4ziGTYD3007LQhd5t8tPB63RxEMqGkho",
-    authDomain: "event-planner-es.firebaseapp.com",
-    databaseURL: "https://event-planner-es.firebaseio.com/"
-  };
